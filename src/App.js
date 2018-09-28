3a77c5524cc47c880ae72068803e288b2db678af
lucazweb <lucas.design@globo.com>
2018-09-27 23:20:23 -0300

    HTML/styled-components, estilos principais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 import './css/global';
+import { AppBox, StocksPanel, StockDetail, StockChart } from './css/components';
 
 class App extends Component {
   render() {
     return (
-      <div />
+        <AppBox>
+          <div className="header">
+            <h1> Fin.React</h1>
+          </div>
+
+          <div className="main-form">
+            <input className="main-input" placeholder="Ex. aapl" />
+          </div>
+
+          <StocksPanel>
+            <span className="stock-value">224.95</span>
+            <span> + 4.53 (+2.06%) </span>
+          </StocksPanel>
+
+          <StockDetail>
+            <li>
+                <strong>Open</strong>
+                <span>220.42</span>
+            </li>
+            <li>
+                <strong>Bid</strong>
+                <span>220.42</span>
+            </li>
+            <li>
+                <strong>Ask</strong>
+                <span>220.42</span>
+            </li>
+            <li>
+                <strong>Open</strong>
+                <span>220.42</span>
+            </li>
+            <li>
+                <strong>Open</strong>
+                <span>220.42</span>
+            </li>
+          </StockDetail>
+
+          <StockChart />
+
+
+
+        </AppBox>
+
+
     );
   }
 }
---
+++ src/css/components.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const AppBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 600px;
+  margin:100px auto;
+  padding: 25px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  box-shadow: 2px 1px 2px #ccc;
+  background: white;
+
+  h1{
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  input{
+    width: 100%;
+    transition: all ease 0.5s;
+    box-shadow: inset 0 1px 2px rgba(10,10,10,.1);
+    font-size: 1.2em;
+    color: #6699cc;
+    text-align: center;
+    padding: 10px 8px;
+    border: 1px solid #f3f3f3;
+    border-radius: 3px;
+    &:focus{
+      border-color: #069;
+      background: white;
+    }
+`;
+
+export const StocksPanel = styled.div`
+  display: flex;
+  width: 70%;
+  text-align: center;
+  justify-content: center;
+  align-items: flex-end;
+  margin:20px auto;
+
+  span{
+    font-size: 1.8em;
+    color: #669966;
+    letter-spacing: 0.2px;
+
+  }
+
+  .stock-value{
+    font-size: 2.8em;
+    color: #6699cc;
+    font-weight: bold;
+    margin-right: 2px;
+  }
+`;
+
+export const StockDetail = styled.ul`
+  display:flex;
+  flex-flow: row wrap;
+  width: 100%;
+  list-style: none;
+
+  li{
+    width: 46%;
+    margin: 2%;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    padding-top: 15px;
+    padding-bottom:5px;
+    font-size: 0.8em;
+
+    &:hover{
+      color: #6699cc;
+      border-color: #6699cc;
+    }
+  }
+
+`;
+
+export const StockChart = styled.div`
+  display: flex;
+  width: 100%;
+  min-height: 300px;
+  margin-top: 10px;
+  background: #f3f3f3;
+
+`;
