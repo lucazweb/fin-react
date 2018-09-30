b6b7f072db43ad0a89fb6f57c985f6dc18f78e81
lucazweb <lucas.design@globo.com>
2018-09-29 21:12:36 -0300

    Importando Google Fonts

--- src/css/global.js
+++ src/css/global.js
@@ -1,6 +1,7 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700');
   * {
     margin:0;
     padding:0;
@@ -13,6 +14,10 @@ injectGlobal`
     text-rendering: optimizeLegibility !important;
     -webkkit-font-smoothing: antialiased !important;
     font-family: sans-serif;
+  }
+
+  h1, h2, h3, span, p, li, div{
+    font-family: 'Source Sans Pro', sans-serif;
   }
 
 `;
